2e620d0fa7cb412502576ce1178337f2deaea11f
Euclid Ma <maxiaoyao@gmail.com>
2018-12-31 14:05:38 -0800

    fix error

--- MyExecRefsLib/boostTest_LinkedList.cpp
+++ MyExecRefsLib/boostTest_LinkedList.cpp
@@ -2,7 +2,7 @@
 #define BOOST_TEST_MODULE mytests
 #include <boost/test/included/unit_test.hpp>
 #include <iostream>
-#include "LinkedList.h"
+#include "../MathFuncsLib/LinkedList.h"
 #include <cstdlib>
 
 using namespace std;
